9097386f2d35efae23b0db75154d91cfd6d5a9f3
OnTheRoad-6 <1262738748@qq.com>
2024-03-03 16:07:46 +0800

    hot-fix commit

--- src/main/java/com/study/aaa.java
+++ src/main/java/com/study/aaa.java
@@ -4,5 +4,6 @@ public class aaa {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word2");
+        System.out.println("hello hot-fix");
     }
 }
