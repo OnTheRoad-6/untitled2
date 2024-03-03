d8da7f9dae5c77b6a3c125c06138a2d2c72c2816
OnTheRoad-6 <1262738748@qq.com>
2024-03-03 20:44:45 +0800

    local 20:44 commit

--- src/main/java/com/study/aaa.java
+++ src/main/java/com/study/aaa.java
@@ -2,20 +2,7 @@ package com.study;
 
 public class aaa {
     public static void main(String[] args) {
-        System.out.println("hello word");
-        System.out.println("hello word2");
-        System.out.println("hello hot-fix");
-        System.out.println("hot-fix second commit");
-        System.out.println("master 16:12");
-        System.out.println("master 16:22");
-        System.out.println("master 16:23");
-        System.out.println("hot-fix 16:20");
-        System.out.println("hot-fix 16:20");
-        System.out.println("hot-fix 16:21");
-        System.out.println("hot-fix 16:21");
-        System.out.println("merge success");
-        System.out.println("gitHub提交");
-        System.out.println("gitHub提交2");
-        System.out.println("gitHub提交3");
+        System.out.println("20:44");
+
     }
 }
